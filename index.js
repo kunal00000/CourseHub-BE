8916692e3eae37641986ba3736dfd92b79d09590
kunal00000 <kunalverma2468@gmail.com>
2023-07-07 21:11:02 +0530

    feat: adding more cors origin

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(
   cors([
     { origin: "http://127.0.0.1:5173" },
     { origin: "https://main--coursehubadmin.netlify.app" },
+    { origin: "https://coursehubadmin.netlify.app" },
   ])
 );
 
